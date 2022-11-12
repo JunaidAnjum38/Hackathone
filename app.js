2a7ff63682f5c648c2af4dc2c770cd2a4279690b
JunaidAnjum38 <junaidanjumarif@gmail.com>
2022-11-12 17:10:02 +0500

    student form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAuth ,signInWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { getDatabase ,ref, set } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 const firebaseConfig = {
   apiKey: "AIzaSyC7NHpm7fJp9WUTNrIVV2H6tp_hsyeTBBw",
   authDomain: "hackathone-9bae9.firebaseapp.com",
@@ -12,14 +13,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase();
+const db = getDatabase();
 
-let logIn = document.getElementById("login");
-logIn.addEventListener("click" , ()=>{
+
+//login
+function foo(){
     let email = document.getElementById("email")
     let password = document.getElementById("password")
     signInWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
-    // Signed in 
+    .then((userCredential) => {
+        // Signed in 
     const user = userCredential.user;
     console.log("ho gaya log in")
     window.location = "./dashboard/index.html"
@@ -30,22 +34,41 @@ logIn.addEventListener("click" , ()=>{
     const errorMessage = error.message;
     console.log("nhi hua log in")
   });
-})
+}
 
-let logout = document.getElementById("btnOut");
-logout.addEventListener("click", ()=>{
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = user.uid;
-          console.log ("nhi hua")
-          // ...
-        } else {
-          // User is signed out
-          // ...
-          console.log("ho gaya")
-          window.location = "../index.html"
-        }
-      });
-})
+let logIn = document.getElementById("loginID");
+logIn?.addEventListener("click" , foo())
+//data set
+
+// let Name =document.getElementById("name");
+// let F_name =document.getElementById("f_name");
+// let Roll =document.getElementById("rollnum");
+// let Email =document.getElementById("email");
+// let PhoneNO =document.getElementById("phoneNo");
+// let CNIC =document.getElementById("cnic");
+// let Img =document.getElementById("getimg");
+// let Course =document.getElementById("course");
+
+
+function ok(){
+    console.log("nikl")
+    let Name =document.getElementById("name");
+let F_name =document.getElementById("f_name");
+let Roll =document.getElementById("rollnum");
+let Email =document.getElementById("email");
+let PhoneNO =document.getElementById("phoneNo");
+let CNIC =document.getElementById("cnic");
+let Img =document.getElementById("getimg");
+let Course =document.getElementById("course");
+
+// function writeUserData(userId, Name,F_name, Email, img ,Roll,PhoneNO,CNIC,Course) {
+    set(ref(db, 'users/${users.uid}'), {
+      username: Name.value,
+      email: Email.value,
+      profile_picture : img.value,
+    });
+  }
+//}
+
+  let submit =document.getElementById("submit");
+  submit?.addEventListener("click", ok());
